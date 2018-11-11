c44754a6af361c43b2dc1a14c2ee58dbb23a6f2a
shinmyeongcheol <doubleddu@kookmin.ac.kr>
2018-11-11 15:05:05 +0900

    api 배포

--- api/model/database.js
+++ api/model/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const database = new Sequelize(
     'account', // 테이블명
-    'smc', // 아이디
+    'root', // 아이디
     'smc1', // 비밀번호
     {
         host: 'localhost',
