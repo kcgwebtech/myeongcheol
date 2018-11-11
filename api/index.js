e45b134fa6b13308415c9ee9f241427dcb44c533
shinmyeongcheol <doubleddu@kookmin.ac.kr>
2018-11-11 16:04:39 +0900

    커밋

--- api/index.js
+++ api/index.js
@@ -1,11 +1,23 @@
-const express = require('express'); //모듈을 불러오는 구문 //const는 상수
+const express = require('express'); // 모듈을 불러오는 구문
 const app = express(); // express 생성
-const port = 8888; //서버를 열 때 사용되는 포트 정보.
+const port = 8888; // 서버를 열 때 사용되는 포트 정보
+
+const cookieParser = require('cookie-parser');
+const Token = require('./token');
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(Token.checkToken);
+
+app.get('/testAPI', (req, res) => {
+    res.json({
+        data: {
+            message: 'API 테스트 성공',
+        }
+    });
+});
 
 app.use('/auth', require('./auth'));
 
-
-app.listen(port); //포트로 서버 열기
-console.log('start serverwith 8888!');
+app.listen(port); // port로 서버 열기
+console.log('start server with 8888!');
